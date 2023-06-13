87b4e57ae7eff2164dda0fda6bf6adf37cef24d0
Vickman-Design <ndukvictor42@gmail.com>
2023-06-13 11:08:16 +0100

    ella-portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var type = new Typed('.typing-text', {
+    strings : ['web designer', 'front end developer', 'graphics designer', 'marketer'],
+    typeSpeed: 120,
+    loop: true
+});
+
+
+
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () =>{
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}));
